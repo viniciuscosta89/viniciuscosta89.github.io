dccfe32b983d829fd91c7e52e59ece50de8e4e32
Vinicius Costa <vinicius.costa89@gmail.com>
2023-08-09 22:15:24 -0300

    fix: :bug: change adapter to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import pandacss from '@pandacss/dev/astro';
 
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
 	integrations: [react(), pandacss()],
-	adapter: vercel(),
+	adapter: netlify(),
 });
