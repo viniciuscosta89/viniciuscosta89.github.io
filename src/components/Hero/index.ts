5b1e53a2594d88937a87d1706e4948e6d9df0931
Vinicius Costa <vinicius.costa89@gmail.com>
2023-09-05 22:37:23 -0300

    refactor(hero): :recycle: remove unused component

--- src/components/Hero/index.ts
+++ src/components/Hero/index.ts
@@ -1,11 +1,9 @@
 import HeroRoot from './HeroRoot.astro';
-import HeroImage from './HeroImage.astro';
 import HeroTitle from './HeroTitle';
 import HeroText from './HeroText';
 
 export const Hero = {
   Root: HeroRoot,
   Title: HeroTitle,
-  Image: HeroImage,
   Text: HeroText,
 };
