09ee5ee2437dead77588c9ed3003c575f428a687
Vinicius Costa <vinicius.costa89@gmail.com>
2024-03-16 18:03:33 -0300

    refactor: fallback Astro image service to Sharp; updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import astroI18next from 'astro-i18next';
 
@@ -8,8 +8,5 @@ export default defineConfig({
 	i18n: {
 		defaultLocale: 'en-US',
 		locales: ['en-US', 'pt-BR'],
-	},
-	image: {
-		service: squooshImageService(),
 	},
 });
