94602b1105caa8c9f06f264afc7a1ba5d6e83a66
Vinicius Costa <vinicius.costa89@gmail.com>
2025-02-04 00:32:00 -0300

    refactor(projects): :recycle: tailwind migration; SectionTitle migration; selection color

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,22 +1,18 @@
 import { motion } from 'framer-motion';
-import { sectionTitleStyle } from './SectionTitle.styles';
 import type { SectionTitleProps } from './SectionTitle.types';
 
 function SectionTitle({ text, hasMarginBottom, align }: SectionTitleProps) {
-	return (
-		<motion.h2
-			className={sectionTitleStyle({
-				hasMarginBottom: hasMarginBottom,
-				align: align,
-			})}
-			initial={{ opacity: 0, x: -32 }}
-			animate={{ opacity: 1, x: 0 }}
-			transition={{ type: 'spring' }}
-			viewport={{ once: true }}
-		>
-			{text}
-		</motion.h2>
-	);
+  return (
+    <motion.h2
+      className={`text-4 md:text-8 lg:text-9 leading-none font-bold tracking-[-0.071rem] md:tracking-[-0.12781rem] lg:tracking-[-0.15625rem] ${hasMarginBottom ? 'mb-5' : ''} ${align === 'center' ? 'text-center lg:text-left' : ''}`}
+      initial={{ opacity: 0, x: -32 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ type: 'spring' }}
+      viewport={{ once: true }}
+    >
+      {text}
+    </motion.h2>
+  );
 }
 
 export default SectionTitle;
