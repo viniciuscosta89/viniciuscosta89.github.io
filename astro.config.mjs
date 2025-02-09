bee5cb67317f880be0a1dad88c16d36660579d86
Vinicius Costa <vinicius.costa89@gmail.com>
2025-02-09 02:07:27 -0300

    feat: :construction_worker: fix workflow for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://viniciuscosta89.github.io',
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
