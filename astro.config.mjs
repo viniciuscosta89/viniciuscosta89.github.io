8c8d8e21bcf81b3db61efab08c4d692c596484d2
Vinicius Costa <vinicius.costa89@gmail.com>
2025-02-09 02:39:22 -0300

    refactor: :recycle: update Astro to v5.2; updated packages; installed Spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://viniciuscosta89.github.io',
-	integrations: [react()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: 'https://viniciuscosta89.github.io',
+    integrations: [react(), sentry(), spotlightjs()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
